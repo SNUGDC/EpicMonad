e925447d94e9b1206b858c3bf18bda1766c10877
Minjung Kim <Reny.atkins@gmail.com>
2016-01-01 20:37:41 +0900

    Make pathfinder - not completed

---
+++ Assets/PathFinder.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+class TileWithPath
+{
+    public GameObject tile;
+    public List<GameObject> path;
+    public int requireAP;
+    
+    public TileWithPath(GameObject destTile, TileWithPath prevTileWithPath)
+    {
+        this.tile = destTile;
+
+        List<GameObject> prevPath = prevTileWithPath.path;
+        GameObject lastPrevTile = prevTileWithPath.tile;
+        foreach (var prevTile in prevPath)
+            this.path.Add(prevTile);
+        this.path.Add(lastPrevTile);
+
+        this.requireAP += (tile.GetComponent<Tile>().GetRequireAPAtTile() + prevPath.Count);
+    }
+}
+
+public class PathFinder {
+
+    Dictionary<Vector2, GameObject> tiles;
+    Vector2 unitPosition;
+    int remainAP;
+    
+    Dictionary<Vector2, TileWithPath> tilesWithPath;
+    
+    public PathFinder(GameObject unit)
+    {
+        this.unitPosition = unit.GetComponent<Unit>().GetPosition();
+        this.remainAP = unit.GetComponent<Unit>().GetCurrentActionPoint();
+        
+        this.tiles = GameObject.FindObjectOfType<TileManager>().GetAllTiles();
+    }
+    
+    void CalculatePath()
+    {
+        
+    }
+}
